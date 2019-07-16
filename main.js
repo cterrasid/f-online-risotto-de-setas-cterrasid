dbd1b2d90cb6911a4ff8ef8d5c540972164a9f12
cterrasid <clarette.terrasi@gmail.com>
2019-07-16 18:04:52 +0200

    I dont know Javascript :(

--- main.js
+++ main.js
@@ -63,69 +63,64 @@ const subtotalPrice = document.querySelector(".result__subtotal-value");
 const shippingPrice = document.querySelector(".result__shipping-value");
 const totalPrice = document.querySelector(".result__price-total");
 
-const shippingValue = 7;
-
-let itemsQuantity;
-
 // const URL =
 //   "https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json";
 
 // async function request() {
 //   const response = await fetch(URL);
 //   const object = await response.json();
-const recipe = object.recipe;
-const name = recipe.name;
-const title = document.createTextNode(name);
-recipeTitle.appendChild(title);
-const ingredients = object.recipe.ingredients;
-paintElements();
+  const recipe = object.recipe;
+  const name = recipe.name;
+  const title = document.createTextNode(name);
+  recipeTitle.appendChild(title);
+  const ingredients = recipe.ingredients;
+  paintElements();
 
-// ingredientsList.innerHTML += `<li>
-//   <label for="select" title="select">
-//     <input class="select-item" id="select" type="checkbox" value="select" name="select" />
-//   </label>
-//   <label for="quantity" title="quantity">
-//     <input id="quantity" type="number" value=${item} min="0" />
-//   </label>
-//   <div class="product__container">
-//     <h2>${product}</h2>
-//     <p>${brand === undefined ? "Marca no disponible" : brand}</p>
-//     <p>${quantity}</p>
-//   </div>
-//   <div class="price__container">
-//     <p>${price} ${currency}</p>
-//   </div>
-// </li>`;
-
+  // ingredientsList.innerHTML += `<li>
+  //   <label for="select" title="select">
+  //     <input class="select-item" id="select" type="checkbox" value="select" name="select" />
+  //   </label>
+  //   <label for="quantity" title="quantity">
+  //     <input id="quantity" type="number" value=${item} min="0" />
+  //   </label>
+  //   <div class="product__container">
+  //     <h2>${product}</h2>
+  //     <p>${brand === undefined ? "Marca no disponible" : brand}</p>
+  //     <p>${quantity}</p>
+  //   </div>
+  //   <div class="price__container">
+  //     <p>${price} ${currency}</p>
+  //   </div>
+  // </li>`;
 // }
 
 // request();
 
 function paintElements() {
-  ingredients.map(ingredient => {
+  ingredients.map((ingredient, index) => {
     //Create list element
     const listElement = document.createElement("li");
 
     //Create checkbox
     const labelCheckbox = document.createElement("label");
-    labelCheckbox.for = "select";
+    labelCheckbox.for = index;
     labelCheckbox.title = "select";
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "select";
-    inputCheckbox.value = "select";
+    inputCheckbox.id = index;
     inputCheckbox.name = "select";
+    inputCheckbox.addEventListener("change", handleCheckboxChange);
     labelCheckbox.appendChild(inputCheckbox);
 
     //Create input for item quantity
     const labelNumber = document.createElement("label");
-    labelNumber.for = "item";
+    labelNumber.for = index;
     labelNumber.title = "item";
     const inputNumber = document.createElement("input");
     inputNumber.type = "number";
-    inputNumber.id = "item";
-    inputNumber.value = ingredient.items;
+    inputNumber.id = index;
     inputNumber.min = "0";
+    inputNumber.addEventListener("change", handleInputValue);
     labelNumber.appendChild(inputNumber);
 
     // Create product data elements
@@ -135,9 +130,7 @@ function paintElements() {
     productElement.appendChild(product);
 
     const brandElement = document.createElement("p");
-    const brand = document.createTextNode(
-      ingredient.brand === undefined ? "Marca no disponible" : ingredient.brand
-    );
+    const brand = document.createTextNode(ingredient.brand || " ");
     brandElement.appendChild(brand);
 
     const quantityElement = document.createElement("p");
@@ -147,23 +140,52 @@ function paintElements() {
     containerProduct.append(productElement, brandElement, quantityElement);
 
     //Create price data elements
-    const containerPrice = document.createElement("div");
-
     const priceElement = document.createElement("p");
     const price = document.createTextNode(ingredient.price);
     const currency = document.createTextNode(recipe.currency);
     priceElement.append(price, " ", currency);
-
-    containerPrice.appendChild(priceElement);
 
     //Put elements inside the list
     listElement.append(
       labelCheckbox,
       labelNumber,
       containerProduct,
-      containerPrice
+      priceElement
     );
 
     ingredientsList.append(listElement);
+    totalizer();
   });
+}
+
+function totalizer() {
+  let shipping = 7;
+  let countItems = 0;
+  let subtotal = 0;
+
+  ingredients.forEach(ingredient => {
+    // const totalIngredient = inputNumber.value;
+    // countItems += totalIngredient;
+    subtotal += ingredient.price;
+  });
+  totalItems.innerHTML = countItems;
+  subtotalPrice.innerHTML = subtotal;
+  shippingPrice.innerHTML = shipping;
+  totalPrice.innerHTML = subtotal + shipping;
+}
+
+function handleInputValue(e) {
+  const { value } = e.target;
+}
+
+function handleCheckboxChange(e) {
+  const { value } = e.target;
+  const { checked } = e.target;
+  inputNumber.value = value;
+  //si checked es true, item = 1
+  // checked
+  //   ? value === ingredients[checked].items
+  //   : ingredients[checked].items === 0;
+
+  console.log(inputNumber.value);
 }
